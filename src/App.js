60d9661b9f1a1a784042902b4a72f3bde1c2bb49
narek <narekp@playengine.com>
2021-12-04 15:58:59 +0400

    Dynamic skinning values added to root element.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import React, { useEffect } from 'react';
+import {lightTheme} from "./constants/themes";
+
 function App() {
+  const setDefaultThemeColors = () => {
+    const rootEl = document.getElementsByTagName('html')[0];
+    let dynamicVarsStr = '';
+    Object.keys(lightTheme).forEach(
+      (item) => {
+        dynamicVarsStr += `${item}: ${lightTheme[item]}`;
+      }
+    );
+    rootEl.setAttribute('style', dynamicVarsStr);
+  }
+  useEffect(() => setDefaultThemeColors(), []);
   return (
     <div>Hello world!</div>
   );
---
+++ src/constants/access_token.js
@@ -0,0 +1,1 @@
+export const accessToken = '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';
